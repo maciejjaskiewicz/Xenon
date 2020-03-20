45b1890830c65302466e6772d34fc2d81943424c
Maciej Jaśkiewicz <maciek.jaskiewicz97@gmail.com>
2020-03-20 10:57:10 +0100

    Change Catch2 to gtest due to lack of death test

--- tests/src/Dummy.cpp
+++ tests/src/Dummy.cpp
@@ -1,13 +1,13 @@
-#include <catch2/catch.hpp>
+#include <gtest/gtest.h>
 
 #include <vector>
 #include <numeric>
 
-TEST_CASE("Dummy")
+TEST(Dummy, test_dummy)
 {
     std::vector<int> values{ 1, 2, 3 };
 
     const auto sum = std::accumulate(values.begin(), values.end(), 0);
 
-    REQUIRE(sum == 6);
+    EXPECT_EQ(sum, 6);
 }
